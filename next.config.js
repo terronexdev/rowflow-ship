209d1b5dce5f80875ed4edf0964d33f3aaef89bc
terronexdev <terronex.dev@gmail.com>
2026-08-22 11:08:41 -0700

    add root Next.js config for ROWFlow $99

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['vercel.blob.core.windows.net'],
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '10mb',
+    },
+  },
+  webpack: (config) => {
+    config.externals = [...(config.externals || []), { canvas: 'canvas' }];
+    return config;
+  },
+};
+
+module.exports = nextConfig;
